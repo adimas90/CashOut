7cf393b5880d8d477fd3bcfe68b069a1823386e3
Adimas Surya Perdana Putra <adimas.suryap@gmail.com>
2022-06-09 01:49:18 +0700

    Initial Commit

---
+++ CashOutt/CashOuttApp.swift
@@ -0,0 +1,17 @@
+//
+//  CashOuttApp.swift
+//  CashOutt
+//
+//  Created by Adimas Surya Perdana Putra on 09/06/22.
+//
+
+import SwiftUI
+
+@main
+struct CashOuttApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
